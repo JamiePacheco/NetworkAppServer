51901c3bdcd66e253eaffbbd2ac4ad511eddacec
Jamie Pacheco <jamie.pacheco.71@gmail.com>
2025-04-26 12:18:54 -0400

    initalize spring boot application

---
+++ src/main/java/com/app/network/networkapplication/NetworkapplicationApplication.java
@@ -0,0 +1,13 @@
+package com.app.network.networkapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetworkapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetworkapplicationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/network/networkapplication/NetworkapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.network.networkapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetworkapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
